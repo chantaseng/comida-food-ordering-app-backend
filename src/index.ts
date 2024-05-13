54cd81bb20ce2e094140255da780c5d2f054ca03
William Chan <chan.william.ts@gmail.com>
2024-05-13 13:41:26 -0600

    added restaurant search endpoint

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import myUserRoute from './routes/MyUserRoute';
 import { v2 as cloudinary } from 'cloudinary';
 import myRestaurantRoute from './routes/MyRestaurantRoute';
+import restaurantRoute from './routes/RestaurantRoute';
 
 // database
 mongoose
@@ -27,6 +28,7 @@ app.get('/health', async (req: Request, res: Response) => {
 
 app.use('/api/my/user', myUserRoute);
 app.use('/api/my/restaurant', myRestaurantRoute);
+app.use('/api/restaurant', restaurantRoute);
 
 app.listen(7002, () => {
   console.log('server started on localhost:7002');
